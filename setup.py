fe61a8a4c375fd27267067d51c73a6f22b5a335c
alexantoshuk <alexander.antoshuk@gmail.com>
2020-06-02 23:29:51 +0300

    version 0.3.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kbdf',
-    version='0.3.2',
+    version='0.3.3',
     scripts=['scripts/kbdf.py'],
     license='MIT',
     description='Python script to translate text that was typed accidentally in wrong keyboard layout.',
