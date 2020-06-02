87ae887ff92dad14828dbae594063e65df235633
alexantoshuk <alexander.antoshuk@gmail.com>
2020-06-02 15:35:57 +0300

    Add script

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='kbdf',
     version='0.1',
-    packages=find_packages(exclude=['tests*']),
+    scripts=['scripts/kbdf'],
     license='MIT',
     description='Python script to translate text that was typed accidentally in wrong keyboard layout.',
     long_description=open('README.MD').read(),
     install_requires=['pyperclip', 'pyautogui'],
     url='https://github.com/alexantoshuk/kbdf',
     author='Alexander Antoshuk',
-    author_email='alexander.antoshuk@gmail.com'
+    author_email='alexander.antoshuk@gmail.com',
+
+
 )
