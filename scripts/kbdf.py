7ae2a66d0734136562254d56a7f3eac05f156b97
Ali <alexander.antoshuk@gmail.com>
2021-05-21 01:02:27 +0300

    Change keyboard layout switch hotkeys to super+space

--- scripts/kbdf.py
+++ scripts/kbdf.py
@@ -101,7 +101,7 @@ def main(mode='line'):
 
     """ Set Hotkey for Keyboard Layout switch after translation
     """
-    pyautogui.hotkey('altleft', 'shiftleft')  # Uncomment this
+    pyautogui.hotkey('superleft', 'space')  # Uncomment this
     """
     """
 
